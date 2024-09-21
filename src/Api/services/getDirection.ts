efcb48461aad3ecc230cfaf011cc447642246cca
Leeo.exe <leonardo.basso147@gmail.com>
2024-09-21 21:18:19 +0100

    WalkingUseCase-Implementation

--- src/Api/services/getDirection.ts
+++ src/Api/services/getDirection.ts
@@ -1,22 +1,23 @@
-import { LatLng, TransitMode } from "@googlemaps/google-maps-services-js";
+import { LatLng,  TransitMode, TravelMode } from "@googlemaps/google-maps-services-js";
 import { client } from "../loadClient";
 import 'dotenv/config'
 
-export async function getDirection(origin: LatLng, destination: LatLng, transitMode: TransitMode[]) {
-    const keyApi = process.env.API_KEY
+export async function getDirection(origin: LatLng, destination: LatLng, travelMode?: TravelMode) {
+    const key = process.env.API_KEY
 
-    if (!keyApi) {
+    if (!key) {
         throw new Error('Api Invalid !')
     }
 
     const { data } = await client.directions({
         params: {
-            key: keyApi,
+            key,
             origin,
             destination,
+            mode: travelMode
         }
     })
 
 
-    return data
+    return data.routes[0]
 }
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,47 @@
 import express from "express";
 import { getGeoCoded } from "./Api/services/geoCoded";
 import { getDirection } from "./Api/services/getDirection";
+import { z } from 'zod'
+import { TravelMode } from "@googlemaps/google-maps-services-js";
 
 export const app = express();
 
 app.use(express.json()) // To read json requisition
 
+
+
 app.get('/', async (req, res) => {
 
-  const { origin, destination, transitMode } = req.body
+  const GpsBodySchema = z.object({
+    origin: z.string(),
+    destination: z.string(),
+    KmPerLiter: z.number(),
+    bussTicket: z.number(),
+    trainTicket: z.number(),
+    haveBike: z.string()
+  })
+
+
+  const { origin, destination, KmPerLiter, bussTicket, trainTicket, haveBike } = GpsBodySchema.parse(req.body) // Todo: fix travelMode to accept only the TravelMode and TransitMode[] enum
 
   const originLatlng = await getGeoCoded(origin)
   const destinationLatLng = await getGeoCoded(destination)
 
-  const direction = await getDirection(originLatlng, destinationLatLng, transitMode )
+  const { legs } = await getDirection(originLatlng, destinationLatLng)
 
-  
-  res.send(direction)
+  const travelDurationInSeconds = legs[0].duration.value
+
+
+  if (travelDurationInSeconds < 1500) {
+    const travelModeBike = await getDirection(originLatlng, destinationLatLng, TravelMode.walking)
+    const timeToGetThere = travelModeBike.legs[0].duration.value / 60
+    res.send({
+      Message: 'For the time obtained, I realize it`s worth walking.',
+      DurationTime: `${Math.round(timeToGetThere)} Minutes`
+    })
+
+  } else if (travelDurationInSeconds > 1500) {
+    res.send('Loading...')
+  }
+
 })
